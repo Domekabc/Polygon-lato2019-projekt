27874efad7494a5ba7a371aeccabf3ff24db7bb1
Domekabc <45439948+Domekabc@users.noreply.github.com>
2019-05-22 13:40:08 +0200

    zatrzymanie ruchu do przodu przy zdeżeniu z przeszkodą

--- TempleRunner/Intermediate/Build/Win64/TempleRunnerEditor/Development/Engine/SharedPCH.Engine.cpp
+++ TempleRunner/Intermediate/Build/Win64/TempleRunnerEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "D:/corpoGame/Nowy folder/TempleRunner/Intermediate/Build/Win64/TempleRunnerEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/Domin/Desktop/Dominik/Polygon 2019l/Polygon-lato2019-projekt/TempleRunner/Intermediate/Build/Win64/TempleRunnerEditor/Development/Engine/SharedPCH.Engine.h"
